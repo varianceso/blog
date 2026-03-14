df143ada82dc204a86697f4af68d763e5fbc6eba
varianceso <15603454+varianceso@users.noreply.github.com>
2026-03-14 14:43:24 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 cd /var/www/blog
 
-# 先保存index.html副本
-if [ -f public/index.html ]; then
-    cp public/index.html /tmp/index.html.bak
-fi
-
-# 清理
 hexo clean
-
-# 生成
 hexo generate
 
-# 恢复index.html并复制到categories和tags
-if [ -f /tmp/index.html.bak ]; then
-    cp /tmp/index.html.bak public/index.html
-    cp public/index.html public/categories/index.html
-    cp public/index.html public/tags/index.html
-fi
+# 创建软链接使分类和标签页面可访问
+ln -sf public/archives/index.html public/categories/index.html
+ln -sf public/archives/index.html public/tags/index.html
 
-# 重启nginx
 systemctl reload nginx
 
 echo 部署完成！
